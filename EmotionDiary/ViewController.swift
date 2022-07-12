361076b377c551b47ae6873d14c5ff5fc6f55fad
sanhan42 <sangmin613@gmail.com>
2022-07-12 09:09:27 +0900

    Add Label Editing Part

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var emotionLableList: [UILabel]!
+    var emotionName = ["행복해 ","사랑해 ", "좋아해 ", "당황해 ", "속상해 ","우울해 ","심심해 ","피곤해 ", "걱정해 "]
+    var emotionNum:[Int] = [0,0,0,0,0,0,0,0,0]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        for i in 0...8 {
+            emotionLableList[i].text = emotionName[i] + String(emotionNum[i])
+        }
         // Do any additional setup after loading the view.
     }
-
-
 }
 
