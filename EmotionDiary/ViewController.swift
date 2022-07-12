d4f97221275b636d3eb2be728477ec8cac7370cf
sanhan42 <sangmin613@gmail.com>
2022-07-12 16:12:29 +0900

    Add Action

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -9,15 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var emotionLableList: [UILabel]!
+    @IBOutlet var emotionButtonList: [UIButton]!
     var emotionName = ["행복해 ","사랑해 ", "좋아해 ", "당황해 ", "속상해 ","우울해 ","심심해 ","피곤해 ", "걱정해 "]
     var emotionNum:[Int] = [0,0,0,0,0,0,0,0,0]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         for i in 0...8 {
-            emotionLableList[i].text = emotionName[i] + "\(emotionNum[i])"
+            emotionButtonList[i].tag = i
         }
-        // Do any additional setup after loading the view.
     }
+    @IBAction func emotionButtonClicked(_ sender: UIButton) {
+        emotionNum[sender.tag] += 1
+        emotionLableList[sender.tag].text = emotionName[sender.tag] + "\(emotionNum[sender.tag])"
+    }
+    
 }
 
